45161d6f88dac51b2be83792eae62b0fe202b530
Sanjeet Sidhu <sanjeetu3@gmail.com>
2024-05-20 11:21:30 -0700

    updated the pop up menu drop down styles

--- src/components/PopupNavigation.tsx
+++ src/components/PopupNavigation.tsx
@@ -25,10 +25,15 @@ export default function PopupNavigation({
         }, 50)
       }
     >
-      <ul className="top-1/2 left-1/2 bg-slate-200 z-50 right-[calc">
+      <ul className="top-1/2 left-1/2 bg-stone-100 z-50 border-t border-stone-300 text-stone-800">
         {links.map((link) => (
-          <li key={link.text} className="min-w-48">
-            <a href={link.path}>{link.text}</a>
+          <li key={link.text} className="min-w-44 w-full">
+            <a
+              className="w-full flex justify-center p-1 border-stone-300 [&:nth-child(n+1):nth-last-child(n+1)]:border-b border-x "
+              href={link.path}
+            >
+              {link.text}
+            </a>
           </li>
         ))}
       </ul>
@@ -51,8 +56,7 @@ export default function PopupNavigation({
         className={`sm:ml-2.5 text-center px-2  relative flex${(orientation === "vertical" && "w-full py-4") || ""}`}
       >
         <a href="#" data-dropdown="true" id="About">
-          {" "}
-          {text}⌄{" "}
+          {text} <span className="font-bold">▼</span>
         </a>
       </li>
       {visible}
