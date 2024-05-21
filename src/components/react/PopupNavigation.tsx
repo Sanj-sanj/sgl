59be6586f27f65c20b805b07ba41e1c102c80713
Sanj-sanj <sanjeetu3@gmail.com>
2024-05-20 22:31:26 -0700

    Lots of changes...
    added a bunch of images and used the mainly inside of index.
    
    Footer is now a nice more complete looking componenet
    Layout, Header, Navigation minor changes
    index is a big change list, mostly fine tuning styles and adding images.

---
+++ src/components/react/PopupNavigation.tsx
@@ -0,0 +1,92 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+export default function PopupNavigation({
+  text,
+  subTexts,
+  orientation,
+}: {
+  text: string;
+  subTexts: { text: string; path: string }[];
+  orientation: "horizontal" | "vertical";
+}) {
+  const [visible, setVisible] = useState(<></>);
+  const isHoveringPopup = useRef(false);
+
+  const popUpMenu = (links: { text: string; path: string }[]) =>
+    orientation === "horizontal" ? (
+      <div
+        className="h-[100vh] absolute top-1/2 left-1/2"
+        style={{ transform: "translate(-50%, 2%)" }}
+        onMouseMove={() => (isHoveringPopup.current = true)}
+        onMouseLeave={() =>
+          setTimeout(() => {
+            isHoveringPopup.current = false;
+            setVisible(<></>);
+          }, 50)
+        }
+      >
+        <ul className="top-1/2 left-1/2 bg-stone-100 z-50 border-t border-stone-300 text-stone-800">
+          {links.map((link) => (
+            <li key={link.text} className="min-w-44 w-full">
+              <a
+                className="w-full flex justify-center p-1 hover:bg-stone-200 border-stone-300 [&:nth-child(n+1):nth-last-child(n+1)]:border-b border-x "
+                href={link.path}
+              >
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ) : (
+      //  MOBILE MENU'S DROP DOWN BOX
+      <div
+        className="w-full"
+        onMouseMove={() => (isHoveringPopup.current = true)}
+        onMouseLeave={() =>
+          setTimeout(() => {
+            isHoveringPopup.current = false;
+            setVisible(<></>);
+          }, 50)
+        }
+      >
+        <ul className="bg-stone-100 z-50 border-t border-stone-300 text-stone-800">
+          {links.map((link) => (
+            <li key={link.text} className="w-full">
+              <a
+                className="w-full flex justify-center p-1 hover:bg-stone-200 border-stone-300 [&:nth-child(n+1):nth-last-child(n+1)]:border-b border-x "
+                href={link.path}
+              >
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+
+  return (
+    <div className="relative h-auto sm:w-auto w-full">
+      <li
+        onMouseMove={() => {
+          orientation === "horizontal" && setVisible(popUpMenu(subTexts));
+        }}
+        onMouseLeave={() => {
+          setTimeout(() => {
+            if (isHoveringPopup.current === true) return;
+            else isHoveringPopup.current = false;
+            setVisible(<></>);
+          }, 50);
+        }}
+        onClick={() =>
+          orientation === "vertical" && setVisible(popUpMenu(subTexts))
+        }
+        className={`sm:ml-2.5 text-center px-2  relative flex${(orientation === "vertical" && "w-full py-4") || ""}`}
+      >
+        <button data-dropdown="true" id={text}>
+          {text} <span className="font-bold">▼</span>
+        </button>
+      </li>
+      {visible}
+    </div>
+  );
+}
