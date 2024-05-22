afc4df5141caba388723854e0da99e8ef7b4cb9f
Sanj-sanj <sanjeetu3@gmail.com>
2024-05-21 21:51:16 -0700

    got a working swiper gallery with autoplay and loop

--- src/libs/Slider.js
+++ src/libs/Slider.js
@@ -1,15 +1,25 @@
 import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 2500,
+      loop: true,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: { slidesPerView: 1 },
+      1024: { slidesPerView: 3 },
+    },
+
     centeredSlides: true,
-    modules: [Navigation, Pagination],
+    modules: [Navigation, Pagination, Autoplay],
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
