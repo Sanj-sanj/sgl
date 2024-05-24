153919fba3ed4ffb041e4acb5c2c8915310db986
Sanjeet Sidhu <sanjeetu3@gmail.com>
2024-05-24 16:08:44 -0700

    moved the Contact Form into its own component, used in contact and index.
    Social media button now has working links
    
    General changes to a tags not pointing to the right place and other
    small style changes.

--- src/components/react/SocialMediaButton.tsx
+++ src/components/react/SocialMediaButton.tsx
@@ -10,7 +10,7 @@ export default function SocialMediaButton() {
   const instaButton = useRef<null | HTMLButtonElement>(null);
   const overlay = useRef<null | HTMLDivElement>(null);
 
-  function handleOpenMenu() {
+  function toggleMenu() {
     [mailButton, fbButton, instaButton].forEach((el) => {
       const button = el.current;
       if (button) {
@@ -26,38 +26,60 @@ export default function SocialMediaButton() {
       <div
         ref={overlay}
         className="h-full w-full absolute top-0 z-40 hidden"
-        onClick={() => handleOpenMenu()}
+        onClick={() => toggleMenu()}
       ></div>
       <div className="fixed bottom-16 right-6 flex flex-col items-center z-50">
         <div className=" mb-2 flex flex-col [&_button]:my-2 [&_button]:rounded-full [&_button]:p-2">
-          <button className="bg-green-700 translate-x-96" ref={mailButton}>
-            <img
-              src={mailIcon.src}
-              alt="Mailing address icon"
-              className="w-14 h-14 p-1"
-            />
+          <button
+            className="bg-green-700 translate-x-96"
+            ref={mailButton}
+            onClick={() => toggleMenu()}
+            title="Send us an email!"
+          >
+            <a href="mailto:info@sgltrucking.com">
+              <img
+                src={mailIcon.src}
+                alt="Mailing address icon"
+                className="w-14 h-14 p-1"
+              />
+            </a>
           </button>
-          <button className="bg-blue-500 translate-x-96" ref={fbButton}>
-            <img
-              src={facebookIcon.src}
-              alt="Facebook icon"
-              className="w-14 h-14 p-1"
-            />
+          <button
+            className="bg-blue-500 translate-x-96"
+            ref={fbButton}
+            onClick={() => toggleMenu()}
+            title="Check out our Facebook!"
+          >
+            <a href="https://www.facebook.com/sgltrucking/" target="blank">
+              <img
+                src={facebookIcon.src}
+                alt="Facebook icon"
+                className="w-14 h-14 p-1"
+              />
+            </a>
           </button>
           <button
             className="bg-gradient-to-tr from-orange-400 to-fuchsia-700 translate-x-96"
             ref={instaButton}
+            onClick={() => toggleMenu()}
+            title="Check out our Instagram!"
           >
-            <img
-              src={instaIcon.src}
-              alt="Instagram icon"
-              className="w-14 h-14 p-1"
-            />
+            <a
+              href="https://www.instagram.com/truckingcompanydelta/"
+              target="blanks"
+            >
+              <img
+                src={instaIcon.src}
+                alt="Instagram icon"
+                className="w-14 h-14 p-1"
+              />
+            </a>
           </button>
         </div>
         <button
-          className="bg-orange-500  w-20 h-20 p-2 flex justify-center items-center rounded-full"
-          onClick={() => handleOpenMenu()}
+          className="bg-orange-500 w-12 h-12 sm:w-20 sm:h-20 sm:p-2 flex justify-center items-center rounded-full"
+          onClick={() => toggleMenu()}
+          title="Looking for another way to reach us?"
         >
           <img
             src={socialIcon.src}
