a1cd8ba76854b145eaf2ae77043aceed2c230bfd
Sanjeet Sidhu <sanjeetu3@gmail.com>
2024-05-24 12:11:44 -0700

    added a social media button using react, uses refs to handle the displaying of the button.
    i want to change the way buttons become visible so they slide in from
    the right rather than fade in.

--- src/components/react/SocialMediaButton.tsx
+++ src/components/react/SocialMediaButton.tsx
@@ -2,48 +2,70 @@ import socialIcon from "../../assets/social-icon2.svg";
 import facebookIcon from "../../assets/fb.svg";
 import instaIcon from "../../assets/instagram.svg";
 import mailIcon from "../../assets/mail.svg";
-import { useState } from "react";
+import { Fragment, useRef, useState } from "react";
 
 export default function SocialMediaButton() {
-  const [menuIsVisible, setMenuIsVisible] = useState(false);
+  const fbButton = useRef<null | HTMLButtonElement>(null);
+  const mailButton = useRef<null | HTMLButtonElement>(null);
+  const instaButton = useRef<null | HTMLButtonElement>(null);
+  const overlay = useRef<null | HTMLDivElement>(null);
 
-  console.log(menuIsVisible);
+  function handleOpenMenu() {
+    [mailButton, fbButton, instaButton].forEach((el) => {
+      const button = el.current;
+      if (button) {
+        button.classList.toggle("translate-x-96");
+        button.classList.toggle("animate-fade-in");
+      }
+    });
+    overlay.current?.classList.toggle("hidden");
+  }
 
   return (
-    <div className="fixed bottom-16 right-6 z-20 flex flex-col items-center">
-      <div className=" mb-2 flex flex-col [&_button]:my-2 [&_button]:rounded-full [&_button]:p-2 ">
-        <button className="bg-green-700 translate-x-96">
+    <>
+      <div
+        ref={overlay}
+        className="h-full w-full absolute top-0 z-40 hidden"
+        onClick={() => handleOpenMenu()}
+      ></div>
+      <div className="fixed bottom-16 right-6 flex flex-col items-center z-50">
+        <div className=" mb-2 flex flex-col [&_button]:my-2 [&_button]:rounded-full [&_button]:p-2">
+          <button className="bg-green-700 translate-x-96" ref={mailButton}>
+            <img
+              src={mailIcon.src}
+              alt="Mailing address icon"
+              className="w-14 h-14 p-1"
+            />
+          </button>
+          <button className="bg-blue-500 translate-x-96" ref={fbButton}>
+            <img
+              src={facebookIcon.src}
+              alt="Facebook icon"
+              className="w-14 h-14 p-1"
+            />
+          </button>
+          <button
+            className="bg-gradient-to-tr from-orange-400 to-fuchsia-700 translate-x-96"
+            ref={instaButton}
+          >
+            <img
+              src={instaIcon.src}
+              alt="Instagram icon"
+              className="w-14 h-14 p-1"
+            />
+          </button>
+        </div>
+        <button
+          className="bg-orange-500  w-20 h-20 p-2 flex justify-center items-center rounded-full"
+          onClick={() => handleOpenMenu()}
+        >
           <img
-            src={mailIcon.src}
-            alt="Mailing address icon"
-            className="w-14 h-14 p-1"
-          />
-        </button>
-        <button className="bg-blue-500 translate-x-96">
-          <img
-            src={facebookIcon.src}
-            alt="Facebook icon"
-            className="w-14 h-14 p-1"
-          />
-        </button>
-        <button className="bg-gradient-to-tr from-orange-400 to-fuchsia-700 translate-x-96">
-          <img
-            src={instaIcon.src}
-            alt="Instagram icon"
-            className="w-14 h-14 p-1"
+            src={socialIcon.src}
+            alt="Social media menu button."
+            className="animate-pulse p-2 hover:animate-none"
           />
         </button>
       </div>
-      <button
-        className="bg-orange-500  w-20 h-20 p-2 flex justify-center items-center rounded-full"
-        onClick={() => setMenuIsVisible(!menuIsVisible)}
-      >
-        <img
-          src={socialIcon.src}
-          alt="Social media menu button."
-          className="animate-pulse p-2 hover:animate-none"
-        />
-      </button>
-    </div>
+    </>
   );
 }
