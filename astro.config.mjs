3aee76a1cab6c6a41613570076a0f5e1c4f1a685
Sanj-sanj <sanjeetu3@gmail.com>
2024-05-27 22:19:05 -0700

    added generator and sitemap integration. fixed some spelling error on index, made a robots.txt, adjusted social media buttons uneeded margin on desktop view.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
+  // site: "https://www.sgltrucking.com",
   image: {
     service: squooshImageService(),
   },
